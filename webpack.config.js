1b80a75d3838593fec3617a9400ff2fbfd066943
Marc Malignan <mhmalignan@gmail.com>
2017-06-06 00:47:55 +0200

    fix dev/prod bundles + source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,45 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
-module.exports = {
-  entry: './app/main.jsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    sourceMapFilename: 'bundle.js.map'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: path.join(__dirname, 'app'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract([
-          'css-loader',
-          'sass-loader'
-        ])
-      }
+module.exports = function(env) {
+  const config = {
+    entry: './app/main.jsx',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          include: path.join(__dirname, 'app'),
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract([
+            'css-loader',
+            'sass-loader'
+          ])
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'bundle.css',
+        allChunks: true
+      })
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin({
-      filename: 'bundle.css',
-      allChunks: true
-    })
-  ]
+  };
+
+  if(env === 'production') {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval';
+  }
+
+  return config;
 };
