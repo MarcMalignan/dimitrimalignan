4e1081ea19463cca91f8bd01b85fdd7e9acd4d7c
Marc Malignan <mhmalignan@gmail.com>
2017-06-08 22:07:34 +0200

    Bio : add data + style

--- app/pages/Bio/Bio.jsx
+++ app/pages/Bio/Bio.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Home = () => (
-  <div className="Page Bio" />
-);
+import data from './Bio.data.json';
+
+import './Bio.scss';
+
+const Home = () => {
+  const listParagraphs = data.paragraphs.map((p, index) => {
+    const innerHtml = { __html: p };
+    return (
+      <p key={index} dangerouslySetInnerHTML={innerHtml} />
+    );
+  });
+
+  return (
+    <div className="Page Bio">
+      <div className="Bio-content">{listParagraphs}</div>
+    </div>
+  );
+};
 
 export default Home;
