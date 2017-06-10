0b46fbb2cf0f69481b9fe403574f057e538d54a2
Marc Malignan <mhmalignan@gmail.com>
2017-06-10 19:45:49 +0200

    Agenda : show events list

--- app/pages/Agenda/Agenda.jsx
+++ app/pages/Agenda/Agenda.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 import Page from '../../components/Page/Page';
+import ContentPanel from '../../components/ContentPanel/ContentPanel';
+import data from './Agenda.data.json';
 
-const Agenda = () => (
-  <Page pageName="Agenda" />
-);
+const Agenda = () => {
+  const listEvents = data.map((event, index) => (
+    <li key={index} className="Agenda-list-item">
+      <div id="Agenda-list-item-title">{event.title}</div>
+      <div id="Agenda-list-item-date">{event.date}</div>
+      <div id="Agenda-list-item-location">{event.location}</div>
+      {event.link &&
+      <div id="Agenda-list-item-link">
+        <a href={event.link} target="_blank">{event.link}</a>
+      </div>
+      }
+    </li>
+  ));
+
+  return (
+    <Page pageName="Agenda">
+      <ContentPanel>
+        <ul className="Agenda-list">{listEvents}</ul>
+      </ContentPanel>
+    </Page>
+  );
+};
 
 export default Agenda;
