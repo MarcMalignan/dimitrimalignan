669d2055f56a313864cd9e83610277700adf573a
Marc Malignan <mhmalignan@gmail.com>
2017-06-11 00:06:22 +0200

    lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-var webpack = require('webpack');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
-module.exports = function(env) {
+module.exports = (env) => {
   const config = {
     entry: './app/main.jsx',
     output: {
       path: path.join(__dirname, 'dist'),
-      filename: '[hash].bundle.js'
+      filename: '[hash].bundle.js',
     },
     module: {
       loaders: [
         {
           test: /\.jsx?$/,
           include: path.join(__dirname, 'app'),
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
         {
           test: /\.scss$/,
           loader: ExtractTextPlugin.extract([
             'css-loader',
-            'sass-loader'
-          ])
+            'sass-loader',
+          ]),
         },
         {
           test: /\.jpg$/,
           loader: 'file-loader',
           options: {
-            name: './images/[name].[hash].[ext]'
-          }
-        }
-      ]
+            name: './images/[name].[hash].[ext]',
+          },
+        },
+      ],
     },
     resolve: {
-      extensions: ['.js', '.jsx']
+      extensions: ['.js', '.jsx'],
     },
     plugins: [
       new CleanWebpackPlugin(['dist']),
       new HtmlWebpackPlugin({
-        template: 'index.ejs'
+        template: 'index.ejs',
       }),
       new ExtractTextPlugin({
         filename: '[hash].bundle.css',
-        allChunks: true
+        allChunks: true,
       }),
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-    ]
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ],
   };
 
-  if(env === 'production') {
+  if (env === 'production') {
     config.devtool = 'source-map';
   } else {
     config.devtool = 'eval';
