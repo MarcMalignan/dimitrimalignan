fb0537b52641c6012ec133c258ae7be937f8b83b
Marc Malignan <mhmalignan@gmail.com>
2017-06-10 20:27:54 +0200

    Agenda : update html + init style

--- app/pages/Agenda/Agenda.jsx
+++ app/pages/Agenda/Agenda.jsx
@@ -4,17 +4,22 @@ import Page from '../../components/Page/Page';
 import ContentPanel from '../../components/ContentPanel/ContentPanel';
 import data from './Agenda.data.json';
 
+import './Agenda.scss';
+
 const Agenda = () => {
   const listEvents = data.map((event, index) => (
     <li key={index} className="Agenda-list-item">
-      <div id="Agenda-list-item-title">{event.title}</div>
-      <div id="Agenda-list-item-date">{event.date}</div>
-      <div id="Agenda-list-item-location">{event.location}</div>
-      {event.link &&
-      <div id="Agenda-list-item-link">
-        <a href={event.link} target="_blank">{event.link}</a>
+      <div className="Agenda-list-item-header">
+        <div className="Agenda-list-item-type">{event.type}</div>
+        <div className="Agenda-list-item-title">
+          {event.link ?
+            <a href={event.link} target="_blank">{event.title}</a> :
+            event.title || event.type
+          }
+        </div>
+        <div className="Agenda-list-item-date">{event.date}</div>
       </div>
-      }
+      <div className="Agenda-list-item-location">{event.location}</div>
     </li>
   ));
 
