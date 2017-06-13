60dcfe5f84db87ae5a16cca288af13b5fcd08fc5
Marc Malignan <mhmalignan@gmail.com>
2017-06-14 00:03:40 +0200

    Photos : highres overlay + fullres link

--- app/pages/Photos/Photos.jsx
+++ app/pages/Photos/Photos.jsx
@@ -14,21 +14,56 @@ class Photos extends React.Component {
     };
   }
 
+  setHighRes(highres) {
+    this.setState({ highres });
+  }
+
   listPhotos() {
     return this.state.photos.map((photo, index) => {
       /* eslint-disable global-require, import/no-dynamic-require */
       const thumb = require(`../../../images/gallery/thumbs/${photo.filename}`);
-      const fullres = require(`../../../images/gallery/fullres/${photo.filename}`);
+      // const fullres = require(`../../../images/gallery/fullres/${photo.filename}`);
       /* eslint-enable global-require, import/no-dynamic-require */
 
       return (
         <div key={index} className="Photos-gallery-item">
-          <a className="Photos-gallery-item-wrapper" href={fullres} target="_blank">
+          <div
+            className="Photos-gallery-item-wrapper"
+            onClick={() => this.setHighRes(photo)}
+          >
             <img src={thumb} alt="" />
-          </a>
+          </div>
         </div>
       );
     });
+  }
+
+  renderHighRes() {
+    const photo = this.state.highres;
+
+    if (!photo) return null;
+
+    const src = `../../../images/gallery/fullres/${photo.filename}`;
+    const style = {
+      backgroundImage: `url(${src})`,
+    };
+
+    return (
+      <div className="Photos-highres">
+        <div className="Photos-highres-close">
+          <span onClick={() => this.setHighRes()}>✕</span>
+        </div>
+        <div className="Photos-highres-img" style={style} />
+        <div className="Photos-highres-info">
+          <a
+            className="Photos-highres-info-fullres"
+            href={src}
+            target="_blank"
+          >Voir la version haute définition</a>
+          <div className="Photos-highres-info-copyright">© {photo.copyright}</div>
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -37,6 +72,7 @@ class Photos extends React.Component {
         <ContentPanel>
           <div className="Photos-gallery">{this.listPhotos()}</div>
         </ContentPanel>
+        { this.renderHighRes() }
       </Page>
     );
   }
