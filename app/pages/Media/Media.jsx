1bb1a8081b30f19c8f2c573e7bc34d96c6f6bd74
Marc Malignan <mhmalignan@gmail.com>
2017-06-15 23:18:05 +0200

    Media : dailymotion embed

--- app/pages/Media/Media.jsx
+++ app/pages/Media/Media.jsx
@@ -13,11 +13,26 @@ class Media extends React.Component {
     };
   }
 
+  listVideos() {
+    return this.state.videos.map((video) => {
+      const src = `//www.dailymotion.com/embed/video/${video.id}`;
+      return (
+        <iframe
+          key={video.id}
+          title={video.title}
+          src={src}
+          allowFullScreen="true"
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <Page pageName="Media">
         <ContentPanel>
           <h1>Vidéos</h1>
+          {this.listVideos()}
         </ContentPanel>
       </Page>
     );
