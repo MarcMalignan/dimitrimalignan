5c046568af2db285b7f1677e58f3275cc747b9c3
Marc Malignan <mhmalignan@gmail.com>
2017-07-09 14:06:46 +0200

    init Lang component

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Lang from '../Lang/Lang';
 import Nav from '../Nav/Nav';
 
 import './Header.scss';
@@ -10,6 +11,7 @@ const Header = () => (
     <div className="Header-logo">
       <Link to="/">Dimitri Malignan</Link>
     </div>
+    <Lang />
     <Nav />
   </header>
 );
