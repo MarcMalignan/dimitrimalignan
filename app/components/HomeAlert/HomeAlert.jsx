d8160d9984e4906f5c4af1f18d19ff58062926d7
Marc Malignan <Marc.MALIGNAN@radiofrance.com>
2018-10-18 14:12:20 +0200

    Home : add new cd alert

---
+++ app/components/HomeAlert/HomeAlert.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './HomeAlert.scss';
+
+const HomeAlert = ({ img, imgAlt, imgLink, links, title }) => (
+  <div className="HomeAlert">
+
+    {title && (<h1>{title}</h1>)}
+
+    {img && (
+      <div className="HomeAlert-img">
+        <a href={imgLink} target="_blank">
+          <img src={img} alt={imgAlt} />
+        </a>
+      </div>
+    )}
+
+    <div className="HomeAlert-body">
+      {links.map((link, index) => (
+        <a href={link.url} target="_blank" key={index}>
+          <button>{link.label}</button>
+        </a>
+      ))}
+    </div>
+
+  </div>
+);
+
+HomeAlert.propTypes = {
+  img: PropTypes.string,
+  imgAlt: PropTypes.string,
+  imgLink: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    url: PropTypes.string,
+  })),
+  title: PropTypes.string,
+};
+
+HomeAlert.defaultProps = {
+  img: null,
+  imgAlt: '',
+  imgLink: '',
+  links: [],
+  title: null,
+};
+
+export default HomeAlert;
--- app/pages/Home/Home.jsx
+++ app/pages/Home/Home.jsx
@@ -1,9 +1,58 @@
+/* eslint-disable global-require */
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import commons from '../../commons';
 import Page from '../../components/Page/Page';
+import ContentPanel from '../../components/ContentPanel/ContentPanel';
+import HomeAlert from '../../components/HomeAlert/HomeAlert';
 
-const Home = () => (
-  <Page pageName="Home" />
-);
+const Home = (props) => {
+  const albumImg = require('../../../images/album.jpg');
+  const albumTitle = 'Dimitri Malignan - Robert Schumann / Serge Prokofiev';
+  const albumLink = 'http://www.passavantmusictest.fr/produit/dimitri-malignan-robert-schumann-serge-prokofiev';
+
+  const search = props.location.search;
+  const titleLabel = {
+    fr: 'Nouveau CD disponible',
+    en: 'New CD available',
+  };
+  const discoverLinkLabel = {
+    fr: 'Découvrir le CD',
+    en: 'Discover the CD',
+  };
+
+  const links = [
+    { label: commons.translate(search, discoverLinkLabel), url: albumLink },
+  ];
+
+  return (
+    <Page pageName="Home">
+      <ContentPanel>
+
+        <HomeAlert
+          img={albumImg}
+          imgAlt={albumTitle}
+          imgLink={albumLink}
+          links={links}
+          title={commons.translate(search, titleLabel)}
+        />
+
+      </ContentPanel>
+    </Page>
+  );
+};
+
+Home.propTypes = {
+  location: PropTypes.shape({
+    search: PropTypes.string,
+  }),
+};
+
+Home.defaultProps = {
+  location: {
+    search: '',
+  },
+};
 
 export default Home;
