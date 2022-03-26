231ecb5c21887e1d3be5f76f57b33be9405dfc8a
Marc Malignan <mhmalignan@gmail.com>
2022-03-26 14:41:14 +0100

    add bach album

--- app/pages/Music/Music.jsx
+++ app/pages/Music/Music.jsx
@@ -8,12 +8,23 @@ import Page from '../../components/Page/Page';
 
 import './Music.scss';
 
-import albumImg from '../../../images/album.jpg';
+import imgProkofiev from '../../../images/music/albumProkofiev.jpg';
+import imgBach from '../../../images/music/albumBach.jpg';
 
-// TODO: fetch this from data
-const albumTitle = 'Robert Schumann / Serge Prokofiev';
-const albumLink = 'http://www.passavantmusictest.fr/produit/dimitri-malignan-robert-schumann-serge-prokofiev';
-const spotifyLink = 'https://open.spotify.com/album/7zkkAFajFz8NUJF5EDS5vy?si=ZIVU7kZyT1-H7IJGXIrSnQ';
+const albums = [
+  {
+    title: 'J.S. Bach - Peregrinations',
+    link: 'https://www.editionshortus.com/catalogue_fiche.php?prod_id=279',
+    spotifyLink: 'https://open.spotify.com/album/0Mc7ekom9Msikjrrqz1u0l?si=Nzv-IHnpTduOQfcz6oc91A',
+    img: imgBach,
+  },
+  {
+    title: 'Schumann / Prokofiev',
+    link: 'http://www.passavantmusictest.fr/produit/dimitri-malignan-robert-schumann-serge-prokofiev',
+    spotifyLink: 'https://open.spotify.com/album/7zkkAFajFz8NUJF5EDS5vy?si=ZIVU7kZyT1-H7IJGXIrSnQ',
+    img: imgProkofiev,
+  },
+];
 
 const discoverLinkLabel = {
   fr: 'Découvrir le CD',
@@ -28,22 +39,24 @@ const Music = ({ location: { search } }) => (
   <Page pageName="Music">
     <ContentPanel>
       <Gallery>
-        <GalleryItem className="Album">
-          <a href={albumLink} target="_blank">
-            <div className="Album-title">{albumTitle}</div>
-            <img src={albumImg} alt={albumTitle} />
-          </a>
-          <div className="Album-links">
-            <a className="Album-link" href={albumLink} target="_blank">
-              <button type="button">{commons.translate(search, discoverLinkLabel)}</button>
+        {albums.map(({ title, link, spotifyLink, img }) => (
+          <GalleryItem className="Album">
+            <a href={link} target="_blank">
+              <div className="Album-title">{title}</div>
+              <img src={img} alt={title} />
             </a>
-            <a className="Album-link" href={spotifyLink} target="_blank">
-              <button type="button" className="spotify">
-                {commons.translate(search, listenLinkLabel)}
-              </button>
-            </a>
-          </div>
-        </GalleryItem>
+            <div className="Album-links">
+              <a className="Album-link" href={link} target="_blank">
+                <button type="button">{commons.translate(search, discoverLinkLabel)}</button>
+              </a>
+              <a className="Album-link" href={spotifyLink} target="_blank">
+                <button type="button" className="spotify">
+                  {commons.translate(search, listenLinkLabel)}
+                </button>
+              </a>
+            </div>
+          </GalleryItem>
+        ))}
       </Gallery>
     </ContentPanel>
   </Page>
